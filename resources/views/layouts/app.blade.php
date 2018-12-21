656310390f3ed605f84713069afa133ceb051bf1
yamenarahman <yamen.a.abdulrahman@hotmail.com>
2018-12-21 14:19:58 +0200

    edited urls

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('contacts.index') }}"><i class="fa fa-users"></i> Contacts</a>
+                                <a class="nav-link" href="/contacts"><i class="fa fa-users"></i> Contacts</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
